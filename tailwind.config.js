abfc7151ee386a40ccee54b9812dc02a434cf9a7
tAdithyan <adithyanttech@gmil.com>
2024-05-24 11:30:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require('tailwindcss/colors')
+
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'custom-dark': '#0c2229',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
